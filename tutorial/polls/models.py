cf362590f6d6c512b9960f9e053b5506ac12504e
Saulo Souza <saulo.souza@somagrupo.com.br>
2021-02-20 19:40:14 -0300

    Models Question e Polls + admin ativado

---
+++ tutorial/polls/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=300)
+    pub_date = models.DateTimeField('data de publicação')
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=300)
+    votes = models.IntegerField(default=0)
+        
